118858a71c7533f73110bb299142ff7acd1dae49
goodbuzzer <102923213+goodbuzzer@users.noreply.github.com>
2023-11-03 10:37:11 +0100

    Update Exercice2.py

--- Exercice2.py
+++ Exercice2.py
@@ -7,10 +7,3 @@ def maxProfit(prix):
                 max_profit = profit
     return max_profit
 
-x= [7,1,5,3,6,4]
-y = [7,6,4,3,1]
-z = [10,1,7,5,9]
-
-print(maxProfit(x))
-print(maxProfit(y))
-print(maxProfit(z))
